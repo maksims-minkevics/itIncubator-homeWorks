c02a8ebd350be55b63e7ec4cf1a88f587a95ab62
maksims-minkevics <126521592+maksims-minkevics@users.noreply.github.com>
2024-12-07 16:01:50 +0200

    h06

--- h06/src/index.ts
+++ h06/src/index.ts
@@ -2,8 +2,9 @@ import {app} from "./app";
 import dotenv from "dotenv";
 import {dbRun} from "./app/db";
 dotenv.config();
-
+import { injectSpeedInsights } from '@vercel/speed-insights';
 const startApp = async () => {
+    injectSpeedInsights();
     console.log("app started...")
     await dbRun();
     app.listen(process.env.PORT, () => {
