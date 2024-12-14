158b4bed224497ace125e0b143db34107ec5d11f
maksims-minkevics <126521592+maksims-minkevics@users.noreply.github.com>
2024-12-14 21:09:55 +0200

    h07

--- h07/src/app/email-templates.ts
+++ h07/src/app/email-templates.ts
@@ -4,6 +4,6 @@ dotenv.config()
 export const registrationEmailTemplate = (confirmationCode: string) => `
     <h1>Thanks for your registration</h1>
     <p>To finish registration please follow the link below:
-        <a href='https://it-incubator-home-works.vercel.app/${process.env.BASE_URL}/registration-confirmation/?code=${confirmationCode}'>complete registration</a>
+        <a href='https://it-incubator-home-works.vercel.app${process.env.BASE_URL}registration-confirmation/?code=${confirmationCode}'>complete registration</a>
     </p>
 `;
